fdc3a1ea48261ac99ca8739694e9a0671f57ac67
Roni Blomberg <rblomberg@prasos.fi>
2016-05-02 09:24:09 +0000

    Add formatByte filter

--- public/src/js/filters.js
+++ public/src/js/filters.js
@@ -12,4 +12,22 @@ angular.module('insight')
       var delimiter = delimiter || ',';
       return input.split(delimiter);
     }
+  })
+  .filter('formatByte', function () {
+    return function (size, useBinary) {
+      var base, prefixes;
+
+      if (useBinary) {
+        base = 1024;
+        prefixes = ['Ki','Mi','Gi','Ti','Pi','Ei','Zi','Yi'];
+      }
+      else {
+        base = 1000;
+        prefixes = ['k','M','G','T','P','E','Z','Y'];
+      }
+
+      var exp = Math.log(size) / Math.log(base) | 0;
+      return (size / Math.pow(base, exp)).toFixed(1) + ' ' +
+        ((exp > 0) ? prefixes[exp - 1] + 'B' : 'B');
+    };
   });
